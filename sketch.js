617c14bd5bb4e573b4ae6fad3dc62e56aba0a388
parthdond <77285014+parthdond@users.noreply.github.com>
2021-04-13 15:26:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var fixedRect,movingRect,fixedRect1;
+function setup() {
+  createCanvas(800,400);
+  fixedRect= createSprite(200, 200, 50, 80);
+  movingRect= createSprite(400, 200, 80, 30);
+  fixedRect1 = createSprite(400,200,30,80);
+}
+
+function draw() {
+  background(255,255,255);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+ if( isTouching(fixedRect,movingRect)){
+  
+  movingRect.shapeColor="blue";
+  fixedRect.shapeColor="blue";
+
+ }
+ else if(isTouching(fixedRect1,movingRect)){
+   movingRect.shapeColor="blue";
+    fixedRect1.shapeColor="blue";
+ }
+else{
+  movingRect.shapeColor="green";
+  fixedRect1.shapeColor="green";
+  fixedRect.shapeColor="green";
+}
+
+  drawSprites();
+}
+
+function isTouching(rect1,rect2){
+  if((rect1.x-rect2.x<rect1.width/2+rect2.width/2 )
+  && (rect1.x-rect2.x<rect1.width/2+rect2.width/2 )&&
+  (rect2.y-rect1.y<rect1.height/2+rect2.height/2 )
+  && (rect1.y-rect2.y<rect1.height/2+rect2.height/2 )){
+   
+  return true;
+  }
+  else{
+    
+   return false;
+  
+  }
+
+
+}
